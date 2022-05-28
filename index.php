ed5f4414e85ce23b668446b5d479d89ba3ef65c1
em-3 <69275008+em-3@users.noreply.github.com>
2022-05-28 11:31:20 -0600

    Added favicons

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Miller Pacific Hardwoods</title>
+        <link rel="icon" type="image/png" sizes="32x32" href="imgs/icon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="imgs/icon-16x16.png">
+
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
         <link rel="stylesheet" href="main.css">
